745f31038c0c20201be89ef5183ee6cc97716093
Oluwadamilare Moronkola <dammixyz@gmail.com>
2022-10-03 13:20:52 +0100

    Initial Commit

---
+++ MantasTest/MantasTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MantasTestApp.swift
+//  MantasTest
+//
+//  Created by StuNNer on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MantasTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
