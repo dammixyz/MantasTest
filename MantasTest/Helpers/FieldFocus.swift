d5723e4b9c8871d79250ee3721f1d4c6d3b3fa3d
Oluwadamilare Moronkola <dammixyz@gmail.com>
2023-12-02 03:04:15 +0100

    set color scheme to light mode

---
+++ MantasTest/Helpers/FieldFocus.swift
@@ -0,0 +1,20 @@
+//
+//  FieldFocus.swift
+//  MantasTest
+//
+//  Created by StuNNer on 10/3/22.
+//
+
+import SwiftUI
+
+struct FieldFocus: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FieldFocus_Previews: PreviewProvider {
+    static var previews: some View {
+        FieldFocus()
+    }
+}
